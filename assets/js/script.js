060fee2aa7c9314400298a09d73c06263aeab3e7
Will G <wgrout87@gmail.com>
2022-04-18 14:43:03 -0600

    added 5-Day forecase

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var tempEl = document.querySelector("#temp");
 var windEl = document.querySelector("#wind");
 var humidityEl = document.querySelector("#humidity");
 var uvEl = document.querySelector("#uv");
+var fiveDayForecastEl = document.querySelector("#fiveDayForecast");
 // END QUERY SELECTORS
 
 
@@ -21,25 +22,39 @@ var searchBtnPressed = false;
 
 // BEGIN FUNCTION DECLARATIONS
 var getCurrentCityWeather = function (city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=792e4643b4781b31d71b6337cd249093&units=imperial")
-        .then(function (response) {
-            // request was successful
-            if (response.ok) {
-                response.json()
-                    .then(function (data) {
-                        console.log(data);
-                        if (searchBtnPressed == true) {
-                            createSearchHistoryBtn(city);
-                            searchBtnPressed = false;
-                        }
-                        updateCurrentCity(city);
-                        var lattitude = data.coord.lat;
-                        var longitude = data.coord.lon;
-                        getWeather(lattitude, longitude);
-                        fiveDayForecast(lattitude, longitude);
-                    })
-            }
-        })
+    if (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=792e4643b4781b31d71b6337cd249093&units=imperial")
+            .then(function (response) {
+                // request was successful
+                if (response.ok) {
+                    response.json()
+                        .then(function (data) {
+                            // console.log(data);
+                            if (searchBtnPressed == true) {
+                                createSearchHistoryBtn(city);
+                                searchBtnPressed = false;
+                            }
+                            updateCurrentCity(city);
+                            var lattitude = data.coord.lat;
+                            var longitude = data.coord.lon;
+                            getWeather(lattitude, longitude);
+                            fiveDayForecast(lattitude, longitude);
+                        })
+                }
+
+                else {
+                    alert("City not found")
+                    searchBtnPressed = false;
+                }
+            })
+            .catch(function (error) {
+                alert("Unable to connect to API");
+                searchBtnPressed = false;
+            });
+    }
+    else {
+        searchBtnPressed = false;
+    }
 };
 
 var updateCurrentCity = function (city) {
@@ -54,7 +69,7 @@ var getWeather = function (lat, lon) {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
-                        console.log(data);
+                        // console.log(data);
                         updateCurrentWeather(data);
                     })
             }
@@ -62,12 +77,20 @@ var getWeather = function (lat, lon) {
 };
 
 var fiveDayForecast = function (lat, lon) {
+    removeOldForecast();
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=792e4643b4781b31d71b6337cd249093&units=imperial")
         .then(function (response) {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
                         console.log(data);
+                        for (i = 0; i < data.list.length; i++) {
+                            var dateAndTimeArr = data.list[i].dt_txt.split(" ");
+                            if (dateAndTimeArr[1] == "12:00:00") {
+                                console.log(dateAndTimeArr[0]);
+                                futureForecast(data.list[i], dateAndTimeArr[0]);
+                            }
+                        }
                     })
             }
         })
@@ -78,6 +101,48 @@ var updateCurrentWeather = function (data) {
     windEl.textContent = data.current.wind_speed + " MPH";
     humidityEl.textContent = data.current.humidity + "%";
     uvEl.textContent = data.current.uvi;
+    assessUvi(data.current.uvi);
+};
+
+var assessUvi = function (uvi) {
+    uvEl.classList.remove("bg-success", "bg-warning", "bg-danger");
+    if (parseInt(uvi) <= 2) {
+        uvEl.classList.add("bg-success");
+    }
+
+    else if (parseInt(uvi) <= 7) {
+        uvEl.classList.add("bg-warning");
+    }
+
+    else {
+        uvEl.classList.add("bg-danger");
+    };
+};
+
+var futureForecast = function (arrObj, date) {
+    var futureDayEl = document.createElement("div");
+    futureDayEl.classList.add("bg-dark", "text-light", "rounded", "col-10", "col-lg-2", "my-4", "mx-auto");
+    var futureDateEl = document.createElement("h3");
+    futureDateEl.textContent = date;
+    futureDayEl.appendChild(futureDateEl);
+    var futureTempEl = document.createElement("p");
+    futureTempEl.textContent = "Temp: " + arrObj.main.temp;
+    futureDayEl.appendChild(futureTempEl);
+    var futureWindEl = document.createElement("p");
+    futureWindEl.textContent = "Wind: " + arrObj.wind.speed + " MPH";
+    futureDayEl.appendChild(futureWindEl);
+    futureHumidityEl = document.createElement("p");
+    futureHumidityEl.textContent = "Humidity: " + arrObj.main.humidity + "%";
+    futureDayEl.appendChild(futureHumidityEl);
+    fiveDayForecastEl.appendChild(futureDayEl);
+};
+
+var removeOldForecast = function () {
+    var child = fiveDayForecastEl.lastElementChild;
+    while (child) {
+        fiveDayForecastEl.removeChild(child);
+        child = fiveDayForecastEl.lastElementChild;
+    }
 };
 
 var createSearchHistoryBtn = function (city) {
@@ -100,7 +165,7 @@ var buttonHandler = function (event) {
             searchInputEl.value = "";
             searchBtnPressed = true;
             getCurrentCityWeather(searchedCity);
-            console.log(searchBtnPressed);
+            // console.log(searchBtnPressed);
         };
         if (event.target.classList.contains("btn-secondary")) {
             searchedCity = event.target.textContent;
